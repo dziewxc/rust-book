32fda45d6192019a7dabf0d3181ee1d30319cde7
Agnieszka <aa.pietruczuk@gmail.com>
2020-04-30 20:48:01 +0200

    rectangle project

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ use std::io;
 struct Shorts {
     color: String,
     brand: String,
-    rating: u8
+    rating: u8,
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
 }
 
 fn main() {
     let mut shorts1 = Shorts {
         color: String::from("blue"),
         brand: String::from("C&A"),
-        rating: 1
+        rating: 1,
     };
 
     let mut user_rating = String::new();
@@ -40,26 +46,70 @@ fn main() {
 
     struct Shirt {
         shirt_color: ShirtColor,
-        shirt_brand: String
+        shirt_brand: String,
     }
 
     let shirt = Shirt {
         shirt_color: ShirtColor(1, 1, 1),
-        shirt_brand:  String::from("Mohito")
+        shirt_brand: String::from("Mohito"),
     };
 
     println!("this is my new shirt, its color is: {}", shirt.shirt_color.0);
 
-/*    struct Shoe {
-        size: u8,
-        brand: &str //missing lifetime specifier!
-    }*/
+    /*    struct Shoe {
+            size: u8,
+            brand: &str //missing lifetime specifier!
+        }*/
+
+    let rect = rectangle_project();
 }
 
 fn build_some_shorts(color: String, brand: String) -> Shorts {
     Shorts {
         color, //field init shorthand
         brand,
-        rating: 0
+        rating: 0,
     }
+}
+
+fn rectangle_project() {
+    let width1 = 30;
+    let height1 = 60;
+
+    let area = calculate_area(width1, height1);
+
+    println!("are of rectangle: {}", area);
+
+    //rectangle project with tuples:
+    let rect2 = (20,30);
+    let area2 = calculate_are_from_tuple(rect2);
+
+    println!("tuple rect area: {}", area2);
+
+    //rectangle project from struct
+
+    let rect3 = Rectangle {
+        height: 400,
+        width: 500
+    };
+
+    let area3 = calculate_area_from_struct(&rect3);
+
+    println!("area from struct: {}", area3);
+
+    //debug print format, needs debug derive {:?}
+    println!("take a look at this weird struct right here: {:?}", rect3);
+    println!("a little prettier print: {:#?}", rect3)
+}
+
+fn calculate_area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn calculate_are_from_tuple(tuple_rectangle: (u32, u32)) -> u32 {
+    tuple_rectangle.0 * tuple_rectangle.1
+}
+
+fn calculate_area_from_struct(r: &Rectangle) -> u32 { //we prefer immutable borrow here
+    r.height * r.width
 }
